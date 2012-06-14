1ba55ba5e80cc3057f4dbdcd250c0e470eec17ee
jasminesloan <jasminesloan@fullsail.edu>
2012-06-13 23:30:04 -0400

    Maybe this will work……

--- week3/Gold/js/main.js
+++ week3/Gold/js/main.js
@@ -1,22 +1,20 @@
 // JQUERY VALIDATION FORM
-
-		var parsePurchaseForm = function(data) {
-		//uses form data here;
+			var parsePurchaseForm = function(data) {
+			//uses form data here;	
 			console.log(data);
 		};
 		
-		$(document).bind('pageinit',function(){
-		
-		var pform = $('#purchaseform');
-		
+		$(document).bind('pageinit', function(){
+				
+			var pform = $('#purchaseform');
+			
 			jQuery.validator.messages.required = "Required";
 			pform.validate({
 				invalidHandler: function(form, validator) {},
 				submitHandler: function() {
 					var data = pform.serializeArray();
 					parsePurchaseForm(data);
-		}
-	});
-		
-});
-	
+				}
+			});
+			
+		});
