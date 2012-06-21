30bd9dd37a1aa0fff7cea9470d203314a9a2315e
jasminesloan <jasminesloan@fullsail.edu>
2012-06-21 08:45:36 -0400

    Committing to Repo

---
+++ week4/Gold/js/main.js
@@ -0,0 +1,20 @@
+// JQUERY VALIDATION FORM
+			var parsePurchaseForm = function(data) {
+			//uses form data here;	
+			console.log(data);
+		};
+		
+		$(document).bind('pageinit', function(){
+				
+			var pform = $('#purchaseform');
+			
+			jQuery.validator.messages.required = "Required";
+			pform.validate({
+				invalidHandler: function(form, validator) {},
+				submitHandler: function() {
+					var data = pform.serializeArray();
+					parsePurchaseForm(data);
+				}
+			});
+			
+		});
